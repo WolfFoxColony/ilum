6ce34e989a9c02aa418de5604e623229e473c57a
ilumm <furt.kariina@gmail.com>
2025-05-22 14:36:51 +0300

    add target blank

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,11 +5,11 @@ import './Contact.scss';
 
 const Contact = () => (
     <div className={'contact'}>
-        <a style={{marginBottom: '15px'}} href={'https://www.linkedin.com/in/karina-furt-a396311a8/'}>
+        <a style={{marginBottom: '15px'}} target={'_blank'} href={'https://www.linkedin.com/in/karina-furt-a396311a8/'}>
             <img className={'contact_img'} src={gitHub} alt="gitHub"/>
         </a>
 
-        <a style={{marginBottom: '100px', cursor: 'pointer'}} href={'https://github.com/WolfFoxColony'}>
+        <a style={{marginBottom: '100px', cursor: 'pointer'}} target={'_blank'} href={'https://github.com/WolfFoxColony'}>
             <img className={'contact_img'} src={linkedin} alt="linkedin"/>
         </a>
     </div>
